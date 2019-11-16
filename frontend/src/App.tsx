46a5540335864513a2c9d17e4f364ba87fbb5566
Magnus Sandén <thefreid@gmail.com>
2019-11-16 21:26:49 +0100

    Display static list of players

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,24 +1,59 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.css'
+
+
+type Player = {
+    firstName: string,
+    lastName: string,
+}
+
+type PlayerListProps = {
+    players: Array<Player>,
+}
+
+const players: Array<Player> = [
+    {
+        firstName: "Hampus",
+        lastName: "Linander",
+    },
+    {
+        firstName: "Magnus",
+        lastName: "Sandén",
+    },
+    {
+        firstName: "Eskil",
+        lastName: "Varenius",
+    },
+];
+
+
+const PlayerList = ({players}: PlayerListProps) => {
+    var rows = players.map((player) => (
+            <tr><td>{player.firstName}</td><td>{player.lastName}</td></tr>
+    ))
+    return (
+            <table className="table table-hover">
+              <thead>
+                <th scope="col">First name</th>
+                <th scope="col">Last name</th>
+              </thead>
+              <tbody>
+                {rows}
+              </tbody>
+            </table>
+    )
+}
 
 const App: React.FC = () => {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        Honte
       </header>
+      <PlayerList players={players} />
     </div>
   );
 }
