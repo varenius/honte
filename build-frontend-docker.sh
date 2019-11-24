56791fd9bc009792e372c78ff90fe58e8a8355e6
Hampus Linander <hampus.linander@gmail.com>
2019-11-24 16:39:41 +0100

    Add shebangs

--- build-frontend-docker.sh
+++ build-frontend-docker.sh
@@ -1,3 +1,4 @@
+#!/bin/bash
 docker build \
        -f docker/Dockerfile.frontend \
        -t frontend \
--- run-frontend-docker.sh
+++ run-frontend-docker.sh
@@ -1,3 +1,4 @@
+#!/bin/bash
 docker run \
        --rm -it \
        -v /home/user/prog/honte:/home/user/prog/honte \
