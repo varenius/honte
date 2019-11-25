e520588fb497b381f0867eff03384f70b5847e22
Magnus Sandén <thefreid@gmail.com>
2019-11-25 08:32:30 +0100

    Allow admin of Player model

--- backend/main/models.py
+++ backend/main/models.py
@@ -6,3 +6,6 @@ class Player(models.Model):
     last_name = models.CharField(max_length=30)
     rating = models.IntegerField()
     egd_pin = models.IntegerField()
+
+    def __str__(self):
+        return f"{self.first_name} {self.last_name}"
